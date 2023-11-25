f78ec3680293754635e78214ee485fe474dd3961
CelsonF <celsondev@outlook.com>
2023-11-24 22:47:27 -0300

    first commit

---
+++ src/main/java/br/com/celsonf/todolist/user/UserModel.java
@@ -0,0 +1,12 @@
+package br.com.celsonf.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    
+    private String name;
+    private String userName;
+    private String password;
+
+}
