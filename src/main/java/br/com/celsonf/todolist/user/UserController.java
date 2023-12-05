f71b6085579925eb3fc597dcca8068e3d7ca79f5
CelsonF <celsondev@outlook.com>
2023-12-05 20:48:37 -0300

    adjust classes in project.

--- src/main/java/br/com/celsonf/todolist/user/UserController.java
+++ src/main/java/br/com/celsonf/todolist/user/UserController.java
@@ -1,6 +1,6 @@
 package br.com.celsonf.todolist.user;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
     
+    @Autowired
+    private IUserRepository userRepository;
+
 
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-
-        System.out.println("User Object:\n"+userModel.getName());
+    public UserModel create(@RequestBody UserModel userModel) {
+       UserModel userCreted = this.userRepository.save(userModel);
+       return userCreted;
     }
 
 }
