b3011fb1a9a286e81dac83055f5372c95cd15c47
CelsonF <celsondev@outlook.com>
2023-12-19 00:26:20 -0300

    feat: add filter to check usar creat tasks

--- src/main/java/br/com/celsonf/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/celsonf/todolist/filter/FilterTaskAuth.java
@@ -1,25 +1,42 @@
 package br.com.celsonf.todolist.filter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class FilterTaskAuth implements Filter {
+public class FilterTaskAuth extends OncePerRequestFilter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-        throws IOException, ServletException {
-        //Execute some actions
-        
-        System.out.println("Enter in Filter Chain");
-        chain.doFilter(request, response);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+            
+                var autorization = request.getHeader("Authorization");
+
+                var authEncoded = autorization.substring("Basic".length()).trim();
+            
+                byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+                var authString = new String(authDecode);
+
+                String[] credenctials = authString.split(":");
+                String userName = credenctials[0];
+                String password = credenctials[1];
+
+                System.out.println(userName);
+                System.out.println(password);
+
+            filterChain.doFilter(request, response);
     }
+
+
     
 }
