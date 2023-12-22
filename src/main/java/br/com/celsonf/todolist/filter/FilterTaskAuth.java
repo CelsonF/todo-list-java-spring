4c5c0150917ece0a9fa7b7dad76ed3478e023182
Celson Fernando <29209254+CelsonF@users.noreply.github.com>
2023-12-22 22:11:57 +0000

    fix: Adjust filter to create task.

--- src/main/java/br/com/celsonf/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/celsonf/todolist/filter/FilterTaskAuth.java
@@ -6,20 +6,31 @@ import java.util.Base64;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import br.com.celsonf.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import br.com.celsonf.todolist.user.IUserRepository;
+
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository userRepository;
+
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
             
-                var autorization = request.getHeader("Authorization");
+                var servletPath = request.getServletPath();
+
+                if(servletPath.equals("/tasks/")) {
+
+                    var autorization = request.getHeader("Authorization");
 
                 var authEncoded = autorization.substring("Basic".length()).trim();
             
@@ -33,10 +44,30 @@ public class FilterTaskAuth extends OncePerRequestFilter {
 
                 System.out.println(userName);
                 System.out.println(password);
+                
+                var user = this.userRepository.findByUserName(userName);
+                if (user == null) {
+                    response.sendError(401);
+                } else {
 
-            filterChain.doFilter(request, response);
+
+
+                    var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(),user.getPassword());
+
+                    if(passwordVerify.verified) {
+                        filterChain.doFilter(request,response);
+                    } else {
+                        response.sendError(401);
+                    }
+
+                }
+                
+            } else {
+                filterChain.doFilter(request, response);
+            }
+
+
+
     }
 
-
-    
 }
