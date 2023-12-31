4ccb692387ebc8c0efcce832b3ccce50e01e86a5
CelsonF <celsondev@outlook.com>
2023-12-31 17:38:28 -0300

    feat: Add method update tasks

--- src/main/java/br/com/celsonf/todolist/task/TaskController.java
+++ src/main/java/br/com/celsonf/todolist/task/TaskController.java
@@ -14,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("/tasks")
@@ -49,6 +52,16 @@ public class TaskController {
         var idUser = request.getAttribute("idUser");
         var tasks = this.taskRepository.findByIdUser((UUID)idUser);
         return tasks;
+    }
+
+    @PutMapping("/{id}")
+    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request ,@PathVariable UUID id) {        
+        var idUser = request.getAttribute("idUser");
+        
+        taskModel.setIdUser((UUID) idUser);
+        taskModel.setId(id);
+        
+        return this.taskRepository.save(taskModel);
     }
 
 }
